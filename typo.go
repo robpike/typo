2521491d739a51300121354484661317cb671b70
Rob Pike <r@golang.org>
2013-04-23 16:04:35 -0700

    typo: rewrite to use the origianl algorithm from Morris & Cherry Also add "a" and "i" to w2006.txt - why were they not there?
    R=r
    https://golang.org/cl/8816047

--- typo.go
+++ typo.go
@@ -1,14 +1,19 @@
+// Copyright 2013 The rspace Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 // A test case, with With punctuation. zxyz. Make sure the oddball on this line scores highly. (It does.)
 // Also make sure that "with" shows up doubled despite the case change.
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"math"
 	"os"
+	"runtime"
 	"sort"
 	"strings"
 	"unicode"
@@ -16,6 +21,8 @@ import (
 )
 
 var knownWordsFiles = []string{"words", "w2006.txt"}
+var nTypos = flag.Int("n", 50, "number of words to print")
+var nDoubles = flag.Bool("repeats", true, "show repeated words, like this this")
 
 var (
 	goroot string
@@ -23,58 +30,14 @@ var (
 )
 
 func init() {
-	goroot = os.Getenv("GOROOT")
+	goroot = runtime.GOROOT()
 	gopath = os.Getenv("GOPATH")
 }
-
-type Word struct {
-	word    string
-	file    string
-	lineNum int
-	byteNum int
-	single  float64
-	double  float64
-}
-
-func (w Word) String() string {
-	return fmt.Sprintf("%s:%d:%d %s", w.file, w.lineNum, w.byteNum, w.word)
-}
-
-func (w Word) print() bool {
-	return false
-}
-
-// Sort interface for []*Word
-type Typo []*Word
-
-func (t Typo) Len() int {
-	return len(t)
-}
-
-func (t Typo) Less(i, j int) bool {
-	w1 := t[i]
-	w2 := t[j]
-	return w1.single*w1.double > w2.single*w2.double // sort down
-}
-
-func (t Typo) Swap(i, j int) {
-	t[i], t[j] = t[j], t[i]
-}
-
-var words = make([]Word, 0, 1000)
-var known = make(map[string]bool) // loaded from knownWordsFiles
-
-var (
-	singleCount int
-	single      = make(map[rune]int)
-	doubleCount int
-	double      = make(map[rune]map[rune]int)
-)
 
 func main() {
 	flag.Parse()
 	for _, f := range knownWordsFiles {
-		for _, w := range readLines(getPath(f), nil) {
+		for _, w := range read(getPath(f), nil, bufio.ScanWords) {
 			known[w] = true
 		}
 	}
@@ -110,7 +73,56 @@ func getPath(base string) string {
 	return dir + base
 }
 
-func readLines(file string, fd *os.File) []string {
+type Word struct {
+	text    string  // The original.
+	lower   *string // The word in lower case; may point to original.
+	file    string
+	lineNum int
+	byteNum int
+	score   float64
+}
+
+func (w Word) String() string {
+	return fmt.Sprintf("%s:%d:%d %s", w.file, w.lineNum, w.byteNum, w.text)
+}
+
+// Sort interfaces for []*Word
+type ByScore []*Word
+
+func (t ByScore) Len() int {
+	return len(t)
+}
+
+func (t ByScore) Less(i, j int) bool {
+	w1 := t[i]
+	w2 := t[j]
+	return w1.score < w2.score
+}
+
+func (t ByScore) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+type ByWord []*Word
+
+func (t ByWord) Len() int {
+	return len(t)
+}
+
+func (t ByWord) Less(i, j int) bool {
+	w1 := t[i]
+	w2 := t[j]
+	return w1.text < w2.text
+}
+
+func (t ByWord) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+var words = make([]*Word, 0, 1000)
+var known = make(map[string]bool) // loaded from knownWordsFiles
+
+func read(file string, fd *os.File, split bufio.SplitFunc) []string {
 	if fd == nil {
 		var err error
 		fd, err = os.Open(file)
@@ -118,17 +130,23 @@ func readLines(file string, fd *os.File) []string {
 			fmt.Fprintf(os.Stdout, "typo: %s\n", file, err)
 			os.Exit(2)
 		}
+		defer fd.Close()
 	}
-	bytes, err := ioutil.ReadAll(fd)
-	if err != nil {
+	scanner := bufio.NewScanner(fd)
+	scanner.Split(split)
+	words := make([]string, 0, 1000)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
 		fmt.Fprintf(os.Stdout, "typo: reading %s: %s\n", file, err)
 		os.Exit(2)
 	}
-	return strings.Split(string(bytes), "\n")
+	return words
 }
 
 func add(file string, fd *os.File) {
-	lines := readLines(file, fd)
+	lines := read(file, fd, bufio.ScanLines)
 	for lineNum, line := range lines {
 		inWord := false
 		wordStart := 1
@@ -148,11 +166,11 @@ func add(file string, fd *os.File) {
 	}
 }
 
-func addWord(word, file string, lineNum, byteNum int) {
-	word = strings.TrimFunc(word, unicode.IsPunct)
+func addWord(text, file string, lineNum, byteNum int) {
+	text = strings.TrimFunc(text, unicode.IsPunct)
 	// There must be a letter.
 	hasLetter := false
-	for _, c := range word {
+	for _, c := range text {
 		if unicode.IsLetter(c) {
 			hasLetter = true
 			break
@@ -161,7 +179,19 @@ func addWord(word, file string, lineNum, byteNum int) {
 	if !hasLetter {
 		return
 	}
-	words = append(words, Word{word: word, file: file, lineNum: lineNum, byteNum: byteNum})
+	word := &Word{
+		text:    text,
+		file:    file,
+		lineNum: lineNum,
+		byteNum: byteNum,
+	}
+	if onlyLower(text) {
+		word.lower = &word.text
+	} else {
+		x := strings.ToLower(text)
+		word.lower = &x
+	}
+	words = append(words, word)
 }
 
 func onlyLower(s string) bool {
@@ -173,134 +203,233 @@ func onlyLower(s string) bool {
 	return true
 }
 
-func lower(s string) string {
-	// Avoid allocation if already lower.
-	if onlyLower(s) {
-		return s
-	}
-	return strings.ToLower(s)
-}
-
 func doubles() {
+	if !*nDoubles {
+		return
+	}
 	prev := ""
-	for i := range words {
-		w := lower(words[i].word)
+	for _, word := range words {
+		w := *word.lower
 		if w == prev {
-			fmt.Printf("%s repeats\n", words[i])
+			fmt.Printf("%s repeats\n", word)
 		}
 		prev = w
 	}
 }
 
+type digram [2]rune
+type trigram [3]rune
+
+var diCounts = make(map[digram]int)
+var triCounts = make(map[trigram]int)
+
 func stats() {
-	// Count singles.
-	for _, w := range words {
-		for _, r := range w.word {
-			single[r]++
-			singleCount++
-		}
+	// Compute global digram and trigram counts.
+	for _, word := range words {
+		addDigrams(word.text)
+		scanTrigrams(word.text, incTrigrams)
 	}
-	maxSingle := 0
-	for _, count := range single {
-		if count > maxSingle {
-			maxSingle = count
-		}
-	}
-	// Scores are 1.0 for a word that has only the most common letter/sequence.
-	// We add the cubes of the rarity of each letter or letter pair, so really unlikely
-	// things skew highly.
-	for i := range words {
-		unlikelihood := 0.0
-		w := &words[i]
-		for _, c := range w.word {
-			count := single[c]
-			ratio := float64(maxSingle) / float64(count)
-			unlikelihood += ratio * ratio * ratio
-		}
-		w.single = unlikelihood / float64(maxSingle*utf8.RuneCountInString(w.word))
-	}
-	// Count doubles.
-	for _, w := range words {
-		var prev rune
-		for i, r := range w.word {
-			if i > 0 {
-				m := double[prev]
-				if m == nil {
-					m = make(map[rune]int)
-					double[prev] = m
-				}
-				m[r]++
-				doubleCount++
-			}
-			prev = r
-		}
-	}
-	maxDouble := 0
-	for _, m := range double {
-		for _, count := range m {
-			if count > maxDouble {
-				maxDouble = count
-			}
-		}
-	}
-	for i := range words {
-		w := &words[i]
-		if len(w.word) < 2 {
+	// Compute the score for the word.
+	for _, word := range words {
+		if known[*word.lower] {
 			continue
 		}
-		unlikelihood := 0.
-		var prev rune
-		for i, r := range w.word {
-			if i > 0 {
-				count := double[prev][r]
-				ratio := float64(maxDouble) / float64(count)
-				unlikelihood += ratio * ratio * ratio
-			}
-			prev = r
-		}
-		w.double = unlikelihood / float64(maxDouble*(utf8.RuneCountInString(w.word)-1))
+		word.score = score(word.text)
 	}
 }
 
-func spell() {
-	// Thresholds are empirically and somewhat arbitrarily determined.
-	const threshold = 1.0
-	const productThreshold = 1e7
-	typos := make(Typo, 0, 100)
-	for i := range words {
-		w := &words[i]
-		if known[w.word] {
-			continue
-		}
-		wLower := lower(w.word)
-		// If we know the word or it has no double score, skip it.
-		if known[wLower] {
-			continue
-		}
-		if w.double == 0 {
-			continue
-		}
-		// If the unlikeliness score is low, skip it.
-		if w.single < threshold && w.double < threshold {
-			continue
-		}
-		// Boost its unlikelihood if it's only lower-case letters, possibly after an initial cap,
-		// since those are even likelier to be typos. We want them to show up high in the list.
-		afterFirst := func(s string) string {
-			_, w := utf8.DecodeRuneInString(s)
-			return s[w:]
-		}
-		if onlyLower(w.word) || onlyLower(afterFirst(w.word)) {
-			w.single *= w.single
-			w.double *= w.double
-		}
-		if w.single*w.double < productThreshold {
-			continue
-		}
-		typos = append(typos, w)
+func scanTrigrams(word string, fn func(t trigram)) {
+	// For "once", we have ".on", "onc", "nce", "ce."
+	// Do the first one by hand to prime the pump.
+	rune, wid := utf8.DecodeRuneInString(word)
+	t := trigram{'.', '.', rune}
+	for _, r := range word[wid:] {
+		t[0] = t[1]
+		t[1] = t[2]
+		t[2] = r
+		fn(t)
 	}
-	sort.Sort(typos)
-	for _, w := range typos {
-		fmt.Printf("%s %d\n", w, int(math.Log(w.single*w.double)-10))
+	// At this point, we have "nce"; make "ce.".
+	// If there was only one letter, "a", we have "..a" and this tail will give us ".a.", which is what we want.
+	// Final marker
+	t[0] = t[1]
+	t[1] = t[2]
+	t[2] = '.'
+	fn(t)
+}
+
+func addDigrams(word string) {
+	d := digram{'.', '.'}
+	// For "once", we have ".o", "on", "nc", "ce", "e."
+	for _, r := range word {
+		d[0] = d[1]
+		d[1] = r
+		diCounts[d]++
+	}
+	// Final marker
+	d[0] = d[1]
+	d[1] = '.'
+	diCounts[d]++
+}
+
+func incTrigrams(t trigram) {
+	triCounts[t]++
+}
+
+func triScore(t trigram) float64 {
+	nxy := float64(diCounts[digram{t[0], t[1]}])
+	nyz := float64(diCounts[digram{t[1], t[2]}])
+	nxyz := float64(triCounts[t])
+	// The paper says to use -10 for log(0), but the count should never be zero.
+	if nxy == 0 || nyz == 0 || nxyz == 0 {
+		fmt.Fprintf(os.Stderr, "typo: bad trigram: %x\n", t)
+		return 0
+	}
+	logNxy := math.Log(nxy)
+	logNyz := math.Log(nyz)
+	logNxyz := math.Log(nxyz)
+	return 0.5*(logNxy+logNyz) - logNxyz
+}
+
+func score(word string) float64 {
+	sumOfSquares := 0.0
+	n := 0
+	fn := func(t trigram) {
+		i := triScore(t)
+		sumOfSquares += i * i
+		n++
+	}
+	scanTrigrams(word, fn)
+	return math.Sqrt(sumOfSquares / float64(n))
+}
+
+func spell() {
+	// Unique: show each word only once, and drop known words.
+	sort.Sort(ByWord(words))
+	out := words[0:0]
+	prev := " " // Can't happen
+	for _, word := range words {
+		if word.text == prev {
+			continue
+		}
+		if known[*word.lower] {
+			continue
+		}
+		out = append(out, word)
+		prev = word.text
+	}
+	words = out
+	sort.Sort(ByScore(words))
+	for i := 0; i < *nTypos && i < len(words); i++ {
+		fmt.Println(words[i])
 	}
 }
+
+/*
+Thanks to Doug McIlroy for digging this out for me:
+
+From CSTR 18, Robert Morris and Lorinda L. Cherry, "Computer
+detection of  typographical errors", July 1974.
+
+
+
+... a count is kept of each kind of digram and trigram in the
+document. For example, if the word `once' appears in the text,
+the counts corresponding to each of these five digrams and four
+trigrams are incremented.
+
+
+
+ .o on nc ce e.
+ .on onc nce ce.
+
+
+
+where the `.' signifies the beginning or ending of a word. These
+statistics are retained for later processing.
+
+
+
+... 2726 common technical English words as used at Murray Hill
+... are removed. [The list was created] by processing about one
+million words of technical text appearing in documents produced on
+the UNIX time-sharing system at Murray Hill. The words selected were
+those which have the highest probability of appearing in a given
+document. This is not the same as a list of the most frequently
+occurring words and, in fact, some very rare words occur in the
+table. For example, `murray' and `abstract' are very uncommon words
+in the documents sampled, yet they both appear once in virtually
+every document.
+
+
+
+... a number is attached to each word by consulting the table of
+digrams and trigrams previously produced.
+
+
+
+The number is an index of peculiarity and reflects the likelihood
+of the hypothesis that the trigrams in the given word were produced
+from the same source that produced the trigram table.
+
+
+
+Each trigram in the current word is treated separately. An initial
+and terminal trigram is treated for each word so that the number
+of trigrams in a word becomes equal to the number of characters
+in the word. For each trigram T = (xyz) in the current word,
+the digram and trigram counts from the prepared table are accessed.
+Each such count is reduced by one to remove the effect of the
+current word on the statistics. The resulting counts are n(xy),
+n(yz), and n(xyz). The index i(T) for the trigram T is set  equal to
+
+
+
+     i(T) = (1/2)[log n(xy) + log n(yz)] - log n(xyz)
+
+
+
+This index is invariant with the size of the document, i.e. it needs
+no normalization.  It measures the evidence against the hypothesis
+that the trigram was produced by the same source that produced the
+digram and trigram tables in the sense of References 1 and 2. In the
+case that one of the digram or trigram counts is zero, the log of
+that count is taken to be -10.
+
+
+
+A variety of methods were tried to combine the trigram indices to
+obtain an index for the whole word, including
+
+- the average of trigram indices,
+- the square root of the average of the squares of the trigram
+  indices, and
+- the largest of the trigram indices.
+
+
+
+All were moderately effective; the first tended to swamp out the
+effect of a really strange trigram in a long word and the latter
+was insensitive to a sequence of strange trigrams whose cumulative
+evidence should have made a word suspcious. The second on trial
+appeared to be a satisfactory compromise and was adopted.
+
+
+
+The words with their attached indices are sorted on the index in
+descending order and printed in a pleasing three-column format. [The
+printed integer index values are] normalized in such a way that
+a word with an index greater than 10 contains trigrams that are not
+representative of the remainder of the document. Such a word almost
+certainly appears just once in a document.
+
+...
+
+
+[1] Good, I. j. Probability and the Weighing of Evidence. Charles
+Griffin & Co., London, 1950.
+
+
+
+[2] Kullback, S. and Leibler, R. A. On Information and Sufficiency.
+Annals of Math. Stat., 22 (1951), pp. 79-86*/
