379de2fbd7047d8937508ad122bc15b7b1affe73
Rob Pike <r@golang.org>
2020-03-21 11:37:07 +1100

    typo/unix/typo.c: revert to true original
    Found it in my archives.

--- unix/typo.c
+++ unix/typo.c
@@ -1,35 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
 char number[3];
 int eflg;
 char w2006[100];
-int flg = 0;
+flg 0;
 char realwd[100];
-char *wd = &realwd[1];
+char *wd {&realwd[1]};
 char *fptr;
+char *ffptr &ffbuf;
 char ffbuf[36];
-char *ffptr = &ffbuf[0];
 int	neng;
 int	npr;
-struct {
-	short table[2];	/*keep these four cards in order*/
-	short tab1[26];
-	short tab2[730];
-	char tab3[19684];
-}tab;
+int table[2];	/*keep these four cards in order*/
+int tab1[26];
+int tab2[730];
+char tab3[19684];
 int logtab[256];
 float inctab[256];
 char nwd[100];
 int tot;
 int wtot;
 char *buf[3];
-int file[3];
-int ptr[3];
+file[3];
+ptr[3];
 char *name[3];
-char bsp[2*768];
+bsp[768];
 
 main(argc,argv) int argc; char *argv[]; {
 	char let,lt;
@@ -40,6 +33,7 @@ main(argc,argv) int argc; char *argv[]; {
 	int ii;
 	double log(), exp(), pow();
 
+	nice(-20);
 	inctab[0] = 1;
 	logtab[0] = -10;
 	for(ii=1; ii<256; ii++){
@@ -59,7 +53,7 @@ main(argc,argv) int argc; char *argv[]; {
 		switch(argv[arg][1]) {
 		default:
 			printf("Unrecognizable argument: %c\n",argv[arg][1]);
-				exit(2);
+				exit();
 		case 0:
 		case 'n':
 				neng++;
@@ -72,19 +66,19 @@ main(argc,argv) int argc; char *argv[]; {
 	}
 	if(!neng) {
 		salt = open("/usr/lib/salt",0);
-		er = read(salt,tab.table,21200);
+		er = read(salt,table,21200);
 			if(er != 21200)err("read salt");
 		close(salt);
 		}
 	if((signal(2,1) & 1) != 1)
-		signal(2,unl);
-	name[0] = strdup("/tmp/ttmpa1");
-	name[1] = strdup("/tmp/ttmpa2");
-	name[2] = strdup("/tmp/ttmpa3");
+	signal(2,unl);
+	name[0] = "/usr/tmp/ttmpa1";
+	name[1] = "/usr/tmp/ttmpa2";
+	name[2] = "/usr/tmp/ttmpa3";
 	while((file[0] = open(name[0],1)) > 0){
 		close(file[0]);
-		for(j=0; j < 3; j++)name[j][9]++;
-		if(name[0][9] == 'z')err("creat tmp file");
+		for(j=0; j < 3; j++)name[j][13]++;
+		if(name[0][13] == 'z')err("creat tmp file");
 	}
 	file[0] = creat(name[0],0666);
 	fptr = argv[arg];
@@ -102,24 +96,25 @@ main(argc,argv) int argc; char *argv[]; {
 			k = -1;
 			l = 0;
 			m = 1;
-			if(inctab[tab.table[0]] > (junk=rand()/32768.)) tab.table[0]++;
+			if(inctab[table[0]] > (junk=rand()/32768.)) table[0]++;
 			while(m <= j){
 				c = 27*wd[k++] + wd[l++];
-				if(inctab[tab.tab2[c]&255] > junk) tab.tab2[c]++;
+				if(inctab[tab2[c]] > junk) tab2[c]++;
 				c = 27*c + wd[m++];
-				if(inctab[tab.tab3[c]&255] > junk) tab.tab3[c]++;
+				if(inctab[tab3[c]] > junk) tab3[c]++;
 			}
 			c = 27*wd[k] + wd[l];
-			if(inctab[tab.tab2[c]] > junk) tab.tab2[c]++;
+			if(inctab[tab2[c]] > junk) tab2[c]++;
 		}
 		if(file[2]) close(file[2]);
 	}
 	flsh(0,0);
 	close(file[0]);
 	sw = fork();
-	if(sw == 0){execl("/usr/bin/usort","usort","-o",name[2],name[0],NULL);
-	if(sw == -1)err("fork");
-	er = wait(0);
+		if(sw == 0){execl("/usr/bin/usort","usort","-o",name[2],name[0],0);
+			err("sort"); }
+		if(sw == -1)err("fork");
+	er = wait();
 		if(er != sw)err("probs");
 	file[0] = creat(name[0],0666);
 		if(file[0] < 0)err("creat tmp");
@@ -159,7 +154,7 @@ main(argc,argv) int argc; char *argv[]; {
 		}
 	}
 done:
-	close(file[2]);
+	close(file[2]); 
 	unlink(name[2]);
 	flsh(0,0);
 	close(file[1]);
@@ -176,10 +171,10 @@ done:
 		while(m <= j){
 			tot = 0;
 			c = wd[k++]*27 + wd[l++];
-			tot =+ (logtab[tab.tab2[c]]+logtab[tab.tab2[wd[k]*27+wd[l]]]);
-			tot >>= 1;
+			tot =+ (logtab[tab2[c]]+logtab[tab2[wd[k]*27+wd[l]]]);
+			tot =>> 1;
 			c = c*27 + wd[m++];
-			tot =- logtab[tab.tab3[c] & 0377];
+			tot =- logtab[tab3[c] & 0377];
 			if(tot > wtot) wtot = tot;
 			}
 		if(wtot < 0) wtot = 0;
@@ -192,17 +187,17 @@ done:
 	close(file[0]);
 
 	sw = fork();
-		if(sw == 0){execl("/usr/bin/sort","sort","+0nr", "+1","-o",name[1],name[1]
-			,NULL);
+		if(sw == 0){execl("/bin/sort","sort","+0nr", "+1","-o",name[1],name[1]
+			,0);
 			err("sort"); }
 		if(sw == -1)err("fork");
-	er = wait(0);
+	er = wait();
 		if(er != sw)err("prob");
 
 	sw = fork();
 		if(sw == 0){
 			if(npr) {
-				execl("/bin/cat","cat",name[1],NULL);
+				execl("/bin/cat","cat",name[1],0);
 			} else {
 				i = 0 ;
 				while((c = "Possible typo's in "[i++])!=0)
@@ -211,12 +206,13 @@ done:
 				while((c = fptr[i++]) != 0)
 					*ffptr++ = c;
 				*ffptr = 0;
-				execl("/usr/bin/pr","pr","-3", "-h",ffbuf,name[1],NULL);
+				execl("/bin/pr","pr","-3", "-h",
+				ffbuf,name[1],0);
 				err("pr");
 		}
 	}
 		if(sw == -1)err("fork");
-	er = wait(0);
+	er = wait();
 		if(er != sw)err("prob");
 	unl();
 }
@@ -225,16 +221,14 @@ unl() {
 	register j;
 	j = 2;
 	while(j--)unlink(name[j]);
-	exit(2);
+	exit();
 }
 
 
 err(c) char c[];{
 	register j;
 	printf("cannot %s\n",c);
-	perror(c);
 	unl();
-	exit(2);
 }
 
 get(ifile) int ifile;{
@@ -272,7 +266,6 @@ flsh(ofile,i){
 	return(0);
 prob:
 	printf("write error on t.%d\n",file[ofile]);
-	perror("write");
 	unl();
 }
 
